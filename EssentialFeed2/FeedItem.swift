64810de0a1524d7fc6e41c6f9717df0e05ec7ae5
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-04-18 14:00:19 +0300

    Create the FeedItem model

---
+++ EssentialFeed2/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed2
+//
+//  Created by Andrey Negovskiy on 4/18/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
