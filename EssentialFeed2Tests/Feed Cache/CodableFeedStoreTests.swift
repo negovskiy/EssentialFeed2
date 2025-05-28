d6ba91abe52bc9854f5ec0adca99e94cd4e36861
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-05-28 16:25:37 +0300

    Prove `CodableFeedStore` side effects run serially

--- EssentialFeed2Tests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed2Tests/Feed Cache/CodableFeedStoreTests.swift
@@ -125,6 +125,32 @@ class CodableFeedStoreTests: XCTestCase {
         XCTAssertNotNil(deletionError, "Expected deletion to fail")
     }
     
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        var completedOperations = [XCTestExpectation]()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(uniqueImageFeed().local, Date()) { _ in
+            completedOperations.append(op1)
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCachedFeed { _ in
+            completedOperations.append(op2)
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(uniqueImageFeed().local, Date()) { _ in
+            completedOperations.append(op3)
+            op3.fulfill()
+        }
+        
+        wait(for: [op1, op2, op3], timeout: 5)
+        XCTAssertEqual(completedOperations, [op1, op2, op3], "Expected operations to run serially")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
