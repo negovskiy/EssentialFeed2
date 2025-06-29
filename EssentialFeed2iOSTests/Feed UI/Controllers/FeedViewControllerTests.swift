9648922bea9f6b5c10b811efbf0f1de1ffb0e6ef
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-06-29 16:40:37 +0300

    Extract `LoaderSpy` to its own file

--- EssentialFeed2iOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed2iOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
@@ -352,64 +352,6 @@ final class FeedViewControllerTests: XCTestCase {
             line: line
         )
     }
-    
-    class LoaderSpy: FeedLoader, FeedImageDataLoader {
-        
-        // MARK: - FeedLoader
-        
-        var loadFeedCallCount: Int {
-            feedRequests.count
-        }
-                
-        private var feedRequests: [(FeedLoader.Result) -> Void] = []
-        
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            feedRequests.append(completion)
-        }
-        
-        func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
-            feedRequests[index](.success(feed))
-        }
-        
-        func completeFeedLoadingWithError(at index: Int) {
-            let error = NSError(domain: "an error", code: 0)
-            feedRequests[index](.failure(error))
-        }
-        
-        // MARK: - ImageDataLoader
-        
-        private struct FeedImageDataLoaderTaskSpy: FeedImageDataLoaderTask {
-            let cancelCallback: () -> Void
-            func cancel() {
-                cancelCallback()
-            }
-        }
-        
-        var loadedImageURLs: [URL] {
-            imageRequests.map(\.url)
-        }
-        
-        private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
-        private(set)var cancelledImageURLs: [URL] = []
-        
-        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any FeedImageDataLoaderTask {
-            imageRequests.append((url, completion))
-            
-            return FeedImageDataLoaderTaskSpy { [weak self] in
-                self?.cancelledImageURLs.append(url)
-            }
-        }
-        
-        func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
-            imageRequests[index].completion(.success(imageData))
-        }
-        
-        func completeImageLoadingWithError(at index: Int = 0) {
-            let error = NSError(domain: "an error", code: 0)
-            imageRequests[index].completion(.failure(error))
-        }
-    }
-    
 }
 
 private extension FeedViewController {
