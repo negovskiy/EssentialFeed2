77abed0eb379b885dea3e81048250dc1bd2b3e9a
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-06-29 16:50:35 +0300

    Extract `FeedUIIntegration` asserts test helpers to its own file

--- EssentialFeed2iOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeed2iOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -290,68 +290,7 @@ final class FeedUIIntegrationTests: XCTestCase {
         UIImage.make(withColor: .red).pngData()!
     }
     
-    private func assertThat(
-        _ sut: FeedViewController,
-        isRendering feed: [FeedImage],
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        guard sut.numberOfRenderedFeedImageView() == feed.count else {
-            return XCTFail(
-                "Expected \(feed.count) rendered feed image view(s), but got \(sut.numberOfRenderedFeedImageView()) instead",
-                file: file,
-                line: line
-            )
-        }
-        
-        for (index, image) in feed.enumerated() {
-            assertThat(sut, hasViewConfiguredFor: image, at: index)
-        }
-    }
     
-    private func assertThat(
-        _ sut: FeedViewController,
-        hasViewConfiguredFor image: FeedImage,
-        at index: Int,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        let view = sut.feedImageView(at: index)
-        
-        guard let cell = view as? FeedImageCell else {
-            return XCTFail(
-                "Expected to render a FeedImageCell, but \(String(describing: view)) was rendered.",
-                file: file,
-                line: line
-            )
-        }
-        
-        let shouldLocationBeVisible = image.location != nil
-        
-        XCTAssertEqual(
-            cell.isShowingLocation,
-            shouldLocationBeVisible,
-            "Expected isShowingLocation to be \(shouldLocationBeVisible)",
-            file: file,
-            line: line
-        )
-    
-        XCTAssertEqual(
-            cell.locationText,
-            image.location,
-            "Expected to show the correct location, but \(String(describing: cell.locationText)) was shown.",
-            file: file,
-            line: line
-        )
-        
-        XCTAssertEqual(
-            cell.descriptionText,
-            image.description,
-            "Expected to show the correct description, but \(String(describing: cell.descriptionText)) was shown.",
-            file: file,
-            line: line
-        )
-    }
 }
 
 private extension FeedViewController {
@@ -401,7 +340,7 @@ private extension FeedViewController {
     }
 }
 
-private extension FeedViewController {
+extension FeedViewController {
     func simulateUserInitiatedFeedReload() {
         refreshControl?.simulatePullToRefresh()
     }
@@ -456,7 +395,7 @@ private extension FeedViewController {
 
 
 
-private extension UIButton {
+extension UIButton {
     func simulateTap() {
         allTargets.forEach { target in
             actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
