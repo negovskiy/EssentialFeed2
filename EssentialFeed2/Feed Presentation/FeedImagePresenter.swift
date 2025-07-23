8eb94656de965eb5965704332ebc9159b77b7b38
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-07-23 12:30:38 +0300

    Move `FeedImagePresenter` and collaborators to production

---
+++ EssentialFeed2/Feed Presentation/FeedImagePresenter.swift
@@ -0,0 +1,73 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeed2
+//
+//  Created by Andrey Negovskiy on 7/23/25.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        location != nil
+    }
+}
+
+public protocol FeedImageView {
+    associatedtype Image
+    
+    func display(_ model: FeedImageViewModel<Image>)
+}
+
+public class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    private let imageTransformer: (Data) -> Image?
+    
+    public init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+        self.view = view
+        self.imageTransformer = imageTransformer
+    }
+    
+    public func didStartLoadingImageData(for model: FeedImage) {
+        view.display(
+            .init(
+                description: model.description,
+                location: model.location,
+                image: nil,
+                isLoading: true,
+                shouldRetry: false
+            )
+        )
+    }
+    
+    public func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        let image = imageTransformer(data)
+        view.display(
+            .init(
+                description: model.description,
+                location: model.location,
+                image: image,
+                isLoading: false,
+                shouldRetry: image == nil
+            )
+        )
+    }
+    
+    public func didFinishLoadingImageData(withError error: Error, for model: FeedImage) {
+        view.display(
+            .init(
+                description: model.description,
+                location: model.location,
+                image: nil,
+                isLoading: false,
+                shouldRetry: true
+            )
+        )
+    }
+}
