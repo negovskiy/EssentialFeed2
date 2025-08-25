653875d670010b9d7effc9eb4dfbb3b205bb4558
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-08-25 11:17:06 +0300

    Extract `URLProtocolStub` to its own class

--- EssentialFeed2Tests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed2Tests/Feed API/URLSessionHTTPClientTests.swift
@@ -90,7 +90,9 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
         let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
         let session = URLSession(configuration: configuration)
+        
         let sut = URLSessionHTTPClient(session: session)
         trackForMemoryLeaks(sut)
         return sut
@@ -167,68 +169,5 @@ class URLSessionHTTPClientTests: XCTestCase {
         HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)!
     }
     
-    private class URLProtocolStub: URLProtocol {
-        
-        private struct Stub {
-            let onStartLoading: (URLProtocolStub) -> Void
-        }
-        
-        private static var _stub: Stub?
-        private static var stub: Stub? {
-            get { return queue.sync { _stub } }
-            set { queue.sync { _stub = newValue } }
-        }
-        
-        private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
-        
-        static func stub(data: Data?, response: URLResponse?, error: Error? = nil) {
-            stub = Stub(onStartLoading: { urlProtocol in
-                guard let client = urlProtocol.client else { return }
-                
-                if let data {
-                    client.urlProtocol(urlProtocol, didLoad: data)
-                }
-                
-                if let response {
-                    client.urlProtocol(urlProtocol, didReceive: response, cacheStoragePolicy: .notAllowed)
-                }
-                
-                if let error {
-                    client.urlProtocol(urlProtocol, didFailWithError: error)
-                } else {
-                    client.urlProtocolDidFinishLoading(urlProtocol)
-                }
-            })
-        }
-        
-        static func observeRequests(observer: @escaping (URLRequest) -> Void) {
-            stub = Stub(onStartLoading: { urlProtocol in
-                urlProtocol.client?.urlProtocolDidFinishLoading(urlProtocol)
-                
-                observer(urlProtocol.request)
-            })
-        }
-        
-        static func onStartLoading(observer: @escaping () -> Void) {
-            stub = Stub(onStartLoading: { _ in observer() })
-        }
-        
-        override func startLoading() {
-            URLProtocolStub.stub?.onStartLoading(self)
-        }
-        
-        static func removeStub() {
-            stub = nil
-        }
-        
-        override class func canInit(with request: URLRequest) -> Bool {
-            true
-        }
-        
-        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-            request
-        }
-        
-        override func stopLoading() {}
-    }
+    
 }
