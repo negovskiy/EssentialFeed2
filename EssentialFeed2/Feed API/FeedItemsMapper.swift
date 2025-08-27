0b7773dee029dae6747bc875eb0a9808b7326c43
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-08-27 13:29:38 +0300

    Extract duplicate `HTTPURLResponse.statusCode == 200` validation into `isOk` shared helper method

--- EssentialFeed2/Feed API/FeedItemsMapper.swift
+++ EssentialFeed2/Feed API/FeedItemsMapper.swift
@@ -11,12 +11,10 @@ final class FeedItemsMapper {
     private struct Root: Decodable {
         let items: [RemoteFeedItem]
     }
-    
-    private static var OK_200: Int { 200 }
-    
+        
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard
-            response.statusCode == OK_200,
+            response.isOk,
             let root = try? JSONDecoder().decode(Root.self, from: data)
         else {
             throw RemoteFeedLoader.Error.invalidData
