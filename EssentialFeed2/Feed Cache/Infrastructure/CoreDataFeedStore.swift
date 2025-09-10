e2fabfb0e55a18bd735e5b2344d7f241d3932617
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-09-10 12:34:19 +0300

    Cleanup references to persistence stores to encapsulate whole Core Data stack lifecycle within lifetime of `CoreDataFeedStore` instance

--- EssentialFeed2/Feed Cache/Infrastructure/CoreDataFeedStore.swift
+++ EssentialFeed2/Feed Cache/Infrastructure/CoreDataFeedStore.swift
@@ -24,4 +24,15 @@ public final class CoreDataFeedStore {
             action(context)
         }
     }
+    
+    private func cleanUpReferencesToPersistentStore() {
+        context.performAndWait {
+            let coordinator = context.persistentStoreCoordinator!
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
+    }
+    
+    deinit {
+        cleanUpReferencesToPersistentStore()
+    }
 }
