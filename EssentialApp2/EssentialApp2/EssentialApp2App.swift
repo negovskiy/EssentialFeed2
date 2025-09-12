2639c5de0894664849cec00d1c5c9248695f6d89
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-09-12 16:30:12 +0300

    Add empty `EssentialApp2` project

---
+++ EssentialApp2/EssentialApp2/EssentialApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialApp2App.swift
+//  EssentialApp2
+//
+//  Created by Andrey Negovskiy on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct EssentialApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
