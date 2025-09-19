578226a0b9fbcd3c077cb72f61da7893abcb4ea8
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-09-19 11:25:18 +0300

    Extract `FeedImageDataLoaderStub` helper into a shared scope

--- EssentialApp2/EssentialApp2Tests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialApp2/EssentialApp2Tests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -130,41 +130,4 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         action()
         wait(for: [exp], timeout: 1)
     }
-    
-    private class FeedImageDataLoaderStub: FeedImageDataLoader {
-        private class TaskWrapperSpy: FeedImageDataLoaderTask {
-            var isCanceled = false
-            var isSecond = false
-            
-            func cancel() {
-                isCanceled = true
-            }
-        }
-        
-        let result: FeedImageDataLoader.Result
-        private var task: TaskWrapperSpy?
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        
-        init(result: FeedImageDataLoader.Result) {
-            self.result = result
-        }
-        
-        func loadImageData(
-            from url: URL,
-            completion: @escaping (FeedImageDataLoader.Result) -> Void
-        ) -> any EssentialFeed2.FeedImageDataLoaderTask {
-            self.completion = completion
-            let task = TaskWrapperSpy()
-            self.task = task
-            return task
-        }
-        
-        func completeLoading() {
-            completion?(result)
-        }
-        
-        func isTaskCancelled() -> Bool {
-            task?.isCanceled == true
-        }
-    }
 }
