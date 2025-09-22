73b91ae65d7a3851fcb84582d288c4f8089e4631
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-09-22 12:26:44 +0300

    Add UI Test target for running high level acceptance tests

---
+++ EssentialApp2/EssentialApp2UIAcceptanceTests/EssentialApp2UIAcceptanceTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialApp2UIAcceptanceTests.swift
+//  EssentialApp2UIAcceptanceTests
+//
+//  Created by Andrey Negovskiy on 9/22/25.
+//
+
+import XCTest
+
+final class EssentialApp2UIAcceptanceTests: XCTestCase {
+    
+}
