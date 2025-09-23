886c5d88f1b25c1149be7981c6c31a924dcaaa0d
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-09-23 11:56:00 +0300

    Extract `DataLoadersFactory` into a separate file

---
+++ EssentialApp2/EssentialApp2/DataLoadersFactory.swift
@@ -0,0 +1,72 @@
+//
+//  DataLoadersFactory.swift
+//  EssentialApp2
+//
+//  Created by Andrey Negovskiy on 9/23/25.
+//
+
+import CoreData
+import EssentialFeed2
+
+class DataLoadersFactory {
+    static func makeLoaders() -> (feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
+        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        let remoteClient = makeRemoteClient()
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+        
+        let localStoreURL = NSPersistentContainer
+            .defaultDirectoryURL()
+            .appendingPathComponent("feed-store.sqlite")
+        
+#if DEBUG
+        if CommandLine.arguments.contains("-reset") {
+            try? FileManager.default.removeItem(at: localStoreURL)
+        }
+#endif
+        
+        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+        let localFeedLoader = LocalFeedLoader(currentDate: Date.init, store: localStore)
+        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+        
+        let feedLoader = FeedLoaderWithFallbackComposite(
+            primary: FeedLoaderCacheDecorator(
+                decoratee: remoteFeedLoader,
+                cache: localFeedLoader
+            ),
+            fallback: localFeedLoader
+        )
+        
+        let imageLoader = FeedImageDataLoaderWithFallbackComposite(
+            primary: localImageLoader,
+            fallback: FeedImageDataLoaderCacheDecorator(
+                decoratee: remoteImageLoader,
+                cache: localImageLoader
+            )
+        )
+        
+        return (feedLoader, imageLoader)
+    }
+    
+    private static func makeRemoteClient() -> HTTPClient {
+#if DEBUG
+        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
+            return AlwaysFailingHTTPClient()
+        }
+#endif
+        return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    }
+}
+
+#if DEBUG
+private class AlwaysFailingHTTPClient: HTTPClient {
+    private class Task: HTTPClientTask {
+        func cancel() {}
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> any EssentialFeed2.HTTPClientTask {
+        completion(.failure(NSError(domain: "domain", code: 0)))
+        return Task()
+    }
+}
+#endif
