b599b84611412da3a1db1de9f583aa10edf17c49
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-09-29 12:11:13 +0300

    Assert snapshots for light and dark mode using an iPhone 17 Pro Max configuration

--- EssentialFeed2/EssentialFeed2iOSTests/FeedSnapshotTests.swift
+++ EssentialFeed2/EssentialFeed2iOSTests/FeedSnapshotTests.swift
@@ -15,7 +15,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(emptyFeed())
         
-        assert(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+        assert(snapshot: sut.snapshot(for: .iPhone17ProMax(.light)), named: "EMPTY_FEED_light")
+        assert(snapshot: sut.snapshot(for: .iPhone17ProMax(.dark)), named: "EMPTY_FEED_dark")
     }
     
     func test_feedWithContent() {
@@ -23,7 +24,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(feedWithContent())
         
-        assert(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
+        assert(snapshot: sut.snapshot(for: .iPhone17ProMax(.light)), named: "FEED_WITH_CONTENT_light")
+        assert(snapshot: sut.snapshot(for: .iPhone17ProMax(.dark)), named: "FEED_WITH_CONTENT_dark")
     }
     
     func test_feedWithErrorMessage() {
@@ -31,7 +33,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(.error(message: "This is a\nmulti line\n error message"))
         
-        assert(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
+        assert(snapshot: sut.snapshot(for: .iPhone17ProMax(.light)), named: "FEED_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone17ProMax(.dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
     }
     
     func test_feedWithFailedImageLoading() {
@@ -39,7 +42,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(feedWithFailedImageLoading())
         
-        assert(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+        assert(snapshot: sut.snapshot(for: .iPhone17ProMax(.light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
+        assert(snapshot: sut.snapshot(for: .iPhone17ProMax(.dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
     }
     
     // MARK: - Helpers
@@ -48,6 +52,8 @@ class FeedSnapshotTests: XCTestCase {
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let controller = storyboard.instantiateInitialViewController() as! FeedViewController
         controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
         trackForMemoryLeaks(controller, file: file, line: line)
         return controller
     }
@@ -135,10 +141,39 @@ class FeedSnapshotTests: XCTestCase {
 }
 
 extension UIViewController {
+    func snapshot(for configuration: SnapshotConfiguration) -> Data {
+        SnapshotWindow(configuration: configuration, root: self).snapshot()
+    }
+}
+
+private final class SnapshotWindow: UIWindow {
+    private var configuration: SnapshotConfiguration = .iPhone17ProMax(.light)
+    
+    convenience init(configuration: SnapshotConfiguration, root: UIViewController) {
+        self.init(frame: .init(origin: .zero, size: configuration.size))
+        self.configuration = configuration
+        self.layoutMargins = configuration.layoutMargins
+        self.overrideUserInterfaceStyle = configuration.traitCollection.userInterfaceStyle
+        self.rootViewController = root
+        self.isHidden = false
+        root.view.layoutMargins = configuration.layoutMargins
+    }
+    
+    override var safeAreaInsets: UIEdgeInsets {
+        configuration.safeAreaInsets
+    }
+    
+    func traitCollection() -> UITraitCollection {
+        configuration.traitCollection
+    }
+    
     func snapshot() -> Data {
-        let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
+        let renderer = UIGraphicsImageRenderer(
+            bounds: bounds,
+            format: .init(for: configuration.traitCollection)
+        )
         return renderer.pngData { actions in
-            view.layer.render(in: actions.cgContext)
+            layer.render(in: actions.cgContext)
         }
     }
 }
@@ -172,4 +207,34 @@ private class ImageStub: FeedImageCellControllerDelegate {
     }
     
     func didCancelImageRequest() {}
+}
+
+struct SnapshotConfiguration {
+    // https://developer.apple.com/design/human-interface-guidelines/layout
+    
+    let size: CGSize
+    let safeAreaInsets: UIEdgeInsets
+    let layoutMargins: UIEdgeInsets
+    let traitCollection: UITraitCollection
+    
+    static func iPhone17ProMax(_ style: UIUserInterfaceStyle) -> Self {
+        let traitCollection = UITraitCollection { mutableTraits in
+            mutableTraits.userInterfaceStyle = style
+            mutableTraits.forceTouchCapability = .available
+            mutableTraits.layoutDirection = .leftToRight
+            mutableTraits.preferredContentSizeCategory = .medium
+            mutableTraits.userInterfaceIdiom = .phone
+            mutableTraits.horizontalSizeClass = .compact
+            mutableTraits.verticalSizeClass = .regular
+            mutableTraits.displayScale = 3
+            mutableTraits.displayGamut = .P3
+        }
+
+        return .init(
+            size: .init(width: 440, height: 956),
+            safeAreaInsets: .init(top: 20, left: 0, bottom: 0, right: 0),
+            layoutMargins: .init(top: 20, left: 16, bottom: 0, right: 16),
+            traitCollection: traitCollection
+        )
+    }
 }
