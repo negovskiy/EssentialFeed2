9722032ca2729f60cdff6d2c5986f1a667148cdf
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-09-29 10:51:54 +0300

    Record feed with failed image loading snapshot

--- EssentialFeed2/EssentialFeed2iOSTests/FeedSnapshotTests.swift
+++ EssentialFeed2/EssentialFeed2iOSTests/FeedSnapshotTests.swift
@@ -34,6 +34,14 @@ class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
     }
     
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
@@ -50,16 +58,23 @@ class FeedSnapshotTests: XCTestCase {
     
     private func feedWithContent() -> [ImageStub] {
         [
-            ImageStub(
+            .init(
                 description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
                 location: "East Side Gallery\nMemorial in Berlin, Germany",
                 image: UIImage.make(withColor: .red)
             ),
-            ImageStub(
+            .init(
                 description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
                 location: "Garth Pier",
                 image: UIImage.make(withColor: .green)
             )
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        [
+            .init(description: "", location: "Location", image: nil),
+            .init(description: "", location: "Location", image: nil)
         ]
     }
     
