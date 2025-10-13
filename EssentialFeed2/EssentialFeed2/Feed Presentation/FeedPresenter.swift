d001a6b42089382a28d297140b93f767c3bfd5fa
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-10-13 11:51:05 +0300

    Remove unused logic

--- EssentialFeed2/EssentialFeed2/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed2/EssentialFeed2/Feed Presentation/FeedPresenter.swift
@@ -7,15 +7,7 @@
 
 import Foundation
 
-public protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
 public class FeedPresenter {
-    private let loadingView: ResourceLoadingView
-    private let feedView: FeedView
-    private let errorView: ResourceErrorView
-    
     public static var title: String {
         String(
             localized: "FEED_VIEW_TITLE",
@@ -31,27 +23,6 @@ public class FeedPresenter {
             table: "Shared",
             bundle: Bundle(for: FeedPresenter.self),
             comment: "Error message displayed when we can't load the image feed from the server")
-    }
-    
-    public init(loadingView: ResourceLoadingView, feedView: FeedView, errorView: ResourceErrorView) {
-        self.loadingView = loadingView
-        self.feedView = feedView
-        self.errorView = errorView
-    }
-    
-    public func didStartLoadingFeed() {
-        errorView.display(.noError)
-        loadingView.display(ResourceLoadingViewModel(isLoading: true))
-    }
-    
-    public func didFinishLoadingFeed(_ feed: [FeedImage]) {
-        feedView.display(Self.map(feed))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-    }
-    
-    public func didFinishLoadingFeedWithError(_ error: Error) {
-        errorView.display(.error(message: feedLoadError))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
     }
     
     public static func map(_ feed: [FeedImage]) -> FeedViewModel {
