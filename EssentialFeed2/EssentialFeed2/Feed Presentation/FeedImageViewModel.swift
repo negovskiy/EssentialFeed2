cf14ea7f1979170cde89611c5f02de30124a16b0
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-10-15 10:24:01 +0300

    Remove unused `FeedImagePresenter` logic

--- EssentialFeed2/EssentialFeed2/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed2/EssentialFeed2/Feed Presentation/FeedImageViewModel.swift
@@ -5,12 +5,9 @@
 //  Created by Andrey Negovskiy on 7/23/25.
 //
 
-public struct FeedImageViewModel<Image> {
+public struct FeedImageViewModel {
     public let description: String?
     public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
     
     public var hasLocation: Bool {
         location != nil
