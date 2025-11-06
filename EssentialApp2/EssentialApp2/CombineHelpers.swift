5cddbe709c53193c44349a6646619f3613e1e450
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-11-06 13:46:28 +0200

    Does not load more after loading last page

--- EssentialApp2/EssentialApp2/CombineHelpers.swift
+++ EssentialApp2/EssentialApp2/CombineHelpers.swift
@@ -10,14 +10,27 @@ import Combine
 import EssentialFeed2
 
 public extension Paginated {
+    init(items: [Item], loadMorePublisher: (() -> AnyPublisher<Self, Error>)?) {
+        self.init(items: items, loadMore: loadMorePublisher.map { publisher in
+            return { completion in
+                publisher().subscribe(Subscribers.Sink(receiveCompletion: { result in
+                    if case let .failure(error) = result {
+                        completion(.failure(error))
+                    }
+                }, receiveValue: { result in
+                    completion(.success(result))
+                }))
+            }
+        })
+    }
+    
     var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
-        guard let loadMore else { return nil }
+        guard let loadMore = loadMore else { return nil }
         
         return {
             Deferred {
                 Future(loadMore)
-            }
-            .eraseToAnyPublisher()
+            }.eraseToAnyPublisher()
         }
     }
 }
