75b0d9e6623f1e4cdeda108f7701c88c1d4efe8d
Andrey Negovskiy <a.negovskiy@gmail.com>
2025-11-27 13:21:01 +0200

    Remove deprecated HTTPClient closure based api in favor of the new async one

--- EssentialFeed2/EssentialFeed2/Shared API Infra/URLSessionHTTPClient.swift
+++ EssentialFeed2/EssentialFeed2/Shared API Infra/URLSessionHTTPClient.swift
@@ -7,18 +7,6 @@
 
 import Foundation
 
-private class URLSessionHTTPClientTask: HTTPClientTask {
-    let task: URLSessionDataTask
-    
-    init(task: URLSessionDataTask) {
-        self.task = task
-    }
-    
-    func cancel() {
-        task.cancel()
-    }
-}
-
 public class URLSessionHTTPClient: HTTPClient {
     
     private let session: URLSession
@@ -36,25 +24,5 @@ public class URLSessionHTTPClient: HTTPClient {
         }
         
         return (data, response)
-    }
-    
-    public func get(from url: URL, completion: @Sendable @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-        let task = session.dataTask(with: url) { data, response, error in
-            let result = Result {
-                if let error {
-                    throw error
-                }
-                
-                guard let data, let response = response as? HTTPURLResponse else {
-                    throw UnexpectedValuesRepresentation()
-                }
-                
-                return (data, response)
-            }
-            completion(result)
-        }
-        task.resume()
-        
-        return URLSessionHTTPClientTask(task: task)
     }
 }
